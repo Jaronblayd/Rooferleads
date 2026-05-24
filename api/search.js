a8d91d2b3bc9ddc180f5a87e7447cb40a141a664
Jaronblayd <jaronhodges26@gmail.com>
2026-05-24 11:58:59 -0500

    Update search.js

--- api/search.js
+++ api/search.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           searchStringsArray: [query],
-          maxCrawledPlacesPerSearch: 40,
+          maxCrawledPlacesPerSearch: 20,
           language: "en",
           countryCode: "us",
         }),
